af2ee6e9f3bbf07ec9b4616b1127993ab1e72ecf
awLann <alwanfajrul20@gmail.com>
2024-05-28 15:42:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mentalglow/view/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mentalglow.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mentalglow.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/mentalglow/view/signin/SigninActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mentalglow.view.signin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mentalglow.R
+
+class SigninActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
