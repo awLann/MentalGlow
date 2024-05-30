8fd614c81d94a105cec2c43e5d8984f1a129ada1
awLann <alwanfajrul20@gmail.com>
2024-05-30 10:27:34 +0700

    Develop User Interface

--- app/src/main/java/com/example/mentalglow/view/signin/SigninActivity.kt
+++ app/src/main/java/com/example/mentalglow/view/signin/SigninActivity.kt
@@ -1,12 +1,25 @@
 package com.example.mentalglow.view.signin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.mentalglow.R
+import com.example.mentalglow.databinding.ActivitySigninBinding
+import com.example.mentalglow.view.home.MainActivity
 
 class SigninActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySigninBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin)
+        binding = ActivitySigninBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.btnSignin.setOnClickListener {
+            val intent = Intent(this@SigninActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/mentalglow/view/splash/SplashActivity.kt
+++ app/src/main/java/com/example/mentalglow/view/splash/SplashActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mentalglow.view.main
+package com.example.mentalglow.view.splash
 
 import android.annotation.SuppressLint
 import android.content.Intent
